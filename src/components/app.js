f08127c8e0313f1ffd774594a6cf1a13d3a4621e
simonstroh <simon_stroh@outlook.com>
2018-05-12 16:16:06 -0700

    Autoplay toggle and description extender

--- src/components/app.js
+++ src/components/app.js
@@ -8,14 +8,28 @@ angular.module('video-player')
       this.playerUrl = 'https://www.youtube.com/embed/' + this.videoData[0].id.videoId;
       this.playerTitle = this.videoData[0].snippet.title;
       this.playerDescription = this.videoData[0].snippet.description;
-      // this.theFullCrazyDescription = '';
+      this.autoplay = 'true';
+      this.shortDescription = this.videoData[0].snippet.description;
+      this.detailIsOpen = false;
 
       this.handleClickInParent = (index) => {
         this.videoIdForDetails = this.videoData[parseInt(index)].id.videoId;
-        this.playerUrl = 'https://www.youtube.com/embed/' + this.videoData[parseInt(index)].id.videoId;
+        if (this.autoplay === 'true') {
+          this.playerUrl = 'https://www.youtube.com/embed/' + this.videoData[parseInt(index)].id.videoId + '?autoplay=1';
+        } else {
+          this.playerUrl = 'https://www.youtube.com/embed/' + this.videoData[parseInt(index)].id.videoId;
+        }
         this.playerTitle = this.videoData[parseInt(index)].snippet.title;
         this.playerDescription = this.videoData[parseInt(index)].snippet.description;
       };
+
+      this.changesAutoplay = () => {
+        if (this.autoplay === 'true') {
+          this.autoplay = 'false'
+        } else {
+          this.autoplay = 'true'
+        }
+      }
 
       this.handleInputInParent = (input) => {
         var prompt = { part: 'snippet', key: window.YOUTUBE_API_KEY, type: 'video', videoEmbeddable: 'true' };
@@ -24,26 +38,34 @@ angular.module('video-player')
       };
 
       this.makeDescriptionCrazy = (videoId) => {
-        var options = { part: 'snippet', id: videoId, key: window.YOUTUBE_API_KEY };
-        console.log(videoId);
-        youTubeDetails.result(options, this.newCallback);
-        // this.playerDescription = this.theFullCrazyDescription;
-        // console.log(this.playerDescription);
+        if (this.detailIsOpen) {
+          this.playerDescription = this.shortDescription
+          this.detailIsOpen = false
+        } else {
+          var options = { part: 'snippet', id: videoId, key: window.YOUTUBE_API_KEY };
+          youTubeDetails.result(options, this.newCallback);
+        }
       };
 
       this.updateData = (data) => {
         this.videoData = data.data.items;
         this.videoIdForDetails = this.videoData[0].id.videoId;
-        this.playerUrl = 'https://www.youtube.com/embed/' + this.videoData[0].id.videoId;
+        if (this.autoplay === 'true') {
+          this.playerUrl = 'https://www.youtube.com/embed/' + this.videoData[0].id.videoId + '?autoplay=1';
+        } else {
+          this.playerUrl = 'https://www.youtube.com/embed/' + this.videoData[0].id.videoId;
+        }
         this.playerTitle = this.videoData[0].snippet.title;
         this.playerDescription = this.videoData[0].snippet.description;
       };
 
       this.newCallback = (parameter) => {
-        // this.theFullCrazyDescription = parameter.data.items[0].snippet.description;
+        this.detailIsOpen = true;
+        this.shortDescription = this.playerDescription;
         this.playerDescription = parameter.data.items[0].snippet.description;
-        console.log(parameter);
       };
+      
+      this.handleInputInParent('Space Oddity Chris Hadfield')
     },
   
     templateUrl: 'src/templates/app.html'
